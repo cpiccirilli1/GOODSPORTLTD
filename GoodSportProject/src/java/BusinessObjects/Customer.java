08e48071553ba9a65dd6d300a5a2aa34bb5651d9
cpiccirilli1 <Piccirilli115@gmail.com>
2020-02-24 14:30:06 -0500

    added setters

--- GoodSportProject/src/java/BusinessObjects/Customer.java
+++ GoodSportProject/src/java/BusinessObjects/Customer.java
@@ -8,37 +8,112 @@ import java.sql.*;
  * @author mitho
  */
 public class Customer {
-    String custId;
-    String custPassword;
-    String custFirstName;
-    String custLastName;
-    String custAddress;
-    String custPhone;
-    String custEmail;
+
+    /**
+     * @return the cart
+     */
+    public String getCart() {
+        return cart;
+    }
+
+    /**
+     * @param cart the cart to set
+     */
+    public void setCart(String cart) {
+        this.cart = cart;
+    }
+
+    /**
+     * @return the custId
+     */
+    public String getCustId() {
+        return custId;
+    }
+
+    /**
+     * @param custId the custId to set
+     */
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+
+    /**
+     * @param custPassword the custPassword to set
+     */
+    public void setCustPassword(String custPassword) {
+        this.custPassword = custPassword;
+    }
+
+    /**
+     * @param custFirstName the custFirstName to set
+     */
+    public void setCustFirstName(String custFirstName) {
+        this.custFirstName = custFirstName;
+    }
+
+    /**
+     * @param custLastName the custLastName to set
+     */
+    public void setCustLastName(String custLastName) {
+        this.custLastName = custLastName;
+    }
+
+    /**
+     * @param custAddress the custAddress to set
+     */
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    /**
+     * @param custPhone the custPhone to set
+     */
+    public void setCustPhone(String custPhone) {
+        this.custPhone = custPhone;
+    }
+
+    /**
+     * @param custEmail the custEmail to set
+     */
+    public void setCustEmail(String custEmail) {
+        this.custEmail = custEmail;
+    }
+    private String custId;
+    private String custPassword;
+    private String custFirstName;
+    private String custLastName;
+    private String custAddress;
+    private String custPhone;
+    private String custEmail;
+    private String cart;
     
     public void Customer() {
-        custId = "";
-        custPassword = "";
-        custFirstName = "";
-        custLastName = "";
-        custAddress = "";
-        custPhone = "";
-        custEmail = "";
+        setCustId("");
+        setCustPassword("");
+        setCustFirstName("");
+        setCustLastName("");
+        setCustAddress("");
+        setCustPhone("");
+        setCustEmail("");
+        setCart("");
+       
     }
     
     public void Customer(String Id, String Pswd, String FName, String LName, 
-            String Adr, String Phone, String Email) {
-        custId = Id;
-        custPassword = Pswd;
-        custFirstName = FName;
-        custLastName = LName;
-        custAddress = Adr;
-        custPhone = Phone;
-        custEmail = Email;
+            String Adr, String Phone, String Email, String cart) {
+        setCustId(Id);
+        setCustPassword(Pswd);
+        setCustFirstName(FName);
+        setCustLastName(LName);
+        setCustAddress(Adr);
+        setCustPhone(Phone);
+        setCustEmail(Email);
+        setCart(cart);
     }
     
     public String getId() {
-        return custId;
+        return getCustId();
     }
     
     public String getPassword() {
@@ -85,13 +160,14 @@ public class Customer {
             Statement stmt = connectDB();
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
-                custId = rs.getString("CustID");
-                custFirstName = rs.getString("FirstName");
-                custLastName = rs.getString("LastName");
-                custAddress = rs.getString("Address");
-                custPhone = rs.getString("PhoneNum");
-                custEmail = rs.getString("Email");
-                custPassword = rs.getString("CustPassword");
+                setCustId(rs.getString("CustID"));
+                setCustFirstName(rs.getString("FirstName"));
+                setCustLastName(rs.getString("LastName"));
+                setCustAddress(rs.getString("Address"));
+                setCustPhone(rs.getString("PhoneNum"));
+                setCustEmail(rs.getString("Email"));
+                setCustPassword(rs.getString("CustPassword"));
+                setCart(rs.getString("ShoppingCart"));
             }
         } catch (Exception e) {
             System.out.println(e);
@@ -99,11 +175,11 @@ public class Customer {
     }
     
     public void insertDB(String Id, String Pswd, String FName, 
-            String LName, String Adr, String Phone,  String Email) {
+            String LName, String Adr, String Phone,  String Email, String cart) {
         try {
             String sql = "INSERT INTO Customers VALUES ('" + Id + "', '" + 
                     Pswd + "', '" + FName + "', '" + LName + "', '" + Adr + 
-                    "', '" + Phone + "', '" + Email + "')";
+                    "', '" + Phone + "', '" + Email + "', '" + cart + "')";
             Statement stmt = connectDB();
             stmt.execute(sql);
         } catch (Exception e) {
@@ -111,12 +187,13 @@ public class Customer {
         }
     }
     
-    public void updateDB(String passwd, String fName, String lName, String address, String phone, String eMail) {
+    public void updateDB(String passwd, String fName, String lName, String address, String phone, String eMail, String cart) {
         try {
-            String sql = "UPDATE Customers SET CustID = '" + custId + "', CustPassword = '"
+            String sql = "UPDATE Customers SET CustID = '" + getCustId() + "', CustPassword = '"
                     + passwd + "', FirstName = '" + fName + "', LastName = '" +
                     lName + "', Address = '" + address + "', PhoneNum = '" + phone + "', Email = '" + eMail +
-                    "' WHERE CustID = '" + custId + "'";
+                    "', ShoppingCart='"+ cart +
+                    "' WHERE CustID = '" + getCustId() + "'";
             Statement stmt = connectDB();
             stmt.execute(sql);
         } catch (Exception e) {
@@ -126,7 +203,7 @@ public class Customer {
     
     public void deleteDB() {
         try {
-            String sql = "DELETE FROM Customers WHERE CustID = '" + custId + "'";
+            String sql = "DELETE FROM Customers WHERE CustID = '" + getCustId() + "'";
             Statement stmt = connectDB();
             stmt.execute(sql);
         } catch (Exception e) {
